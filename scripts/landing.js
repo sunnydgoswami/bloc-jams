9e86e0caf995d2d19e985aaf046c140a63c46704
sunnydgoswami <sunnydgoswami@gmail.com>
2017-05-04 00:02:15 -0400

    checkpoint 8 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,6 @@
-var revealPoint = function () {
+var points = document.getElementsByClassName('point');
 
-  var points = document.getElementsByClassName('point');
+var animatePoints = function (points) {
 
   for (var i = 0; i < points.length; i++) {
     points[i].style.opacity = 1
@@ -10,4 +10,18 @@ var revealPoint = function () {
   }
 }
 
-revealPoint();
+window.onload = function () {
+
+   if (window.innerHeight > 950) {
+       animatePoints(pointsArray);
+   }
+
+   var sellingPoints = document.getElementsByClassName('selling-points')[0];
+   var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+   window.addEventListener('scroll', function(event) {
+     if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(points);
+         }
+  });
+}
