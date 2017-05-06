97c85ed3734a9c6b76d521dd20f2cb2e54b04678
sunnydgoswami <sunnydgoswami@gmail.com>
2017-05-06 17:01:57 -0400

    assignment 9 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,14 +1,15 @@
 var points = document.getElementsByClassName('point');
 
-var animatePoints = function (points) {
+var revealPoints = function(point) {
+    point.style.opacity = 1
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)";
+};
 
-  for (var i = 0; i < points.length; i++) {
-    points[i].style.opacity = 1
-    points[i].style.transform = "scaleX(1) translateY(0)";
-    points[i].style.msTransform = "scaleX(1) translateY(0)";
-    points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-  }
-}
+var animatePoints = function (points) {
+  forEach(points, revealPoints);
+};
 
 window.onload = function () {
 
